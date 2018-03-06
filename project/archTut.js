309b86ec5ff5fcafaadbddfd50b6cee37fa6a5c4
Travis Shands <shandst@flip2.engr.oregonstate.edu>
2018-03-05 22:00:11 -0800

    added project

---
+++ project/archTut.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 7017);
+
+app.get('/', function(req,res,next){
+   var context = {};
+   res.render('home', context);
+});
+
+app.use(function(req,res){
+   res.status(404);
+   res.render('404');
+});
+
+app.use(function(err, req, res, next){
+   res.type('plain/text');
+   res.status(500);
+   res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+   console.log('Epress started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
